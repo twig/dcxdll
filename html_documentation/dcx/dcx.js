caee05c226030b59629db66a1608f65cc5f06031
twig <twig@17a06da3-8c20-fa79-550f-e9588e724b77>
2006-04-14 15:46:03 +0000

    - defined version of dcx documentation in dcx.js - documentation <title> tag now dynamic
    git-svn-id: http://dcx.googlecode.com/svn@2 17a06da3-8c20-fa79-550f-e9588e724b77

---
+++ html_documentation/dcx/dcx.js
@@ -0,0 +1,10 @@
+function version() {
+	document.write(v());
+}
+
+function v() {
+	return "1.3.0b";
+}
+
+// changes title for each document
+document.title = "DCX - Dialog Control Xtension DLL v" + v() + " by ClickHeRe";
