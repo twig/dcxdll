820fab5aeeb1abfb179ea7657821a6aca83d6d8e
twig <twig@17a06da3-8c20-fa79-550f-e9588e724b77>
2006-05-26 23:54:56 +0000

    - removed duplicate semicolon - added a note for minmaxsize for FontDialog in dcxdoc
    git-svn-id: http://dcx.googlecode.com/svn@124 17a06da3-8c20-fa79-550f-e9588e724b77

--- docs_new/inc/index.php
+++ docs_new/inc/index.php
@@ -380,6 +380,7 @@ To get hWnd values, use $window().hwnd, $dialog().hwnd, $chan().hwnd, $chat().hw
 			'__notes' => array(
 				'Returns "" when cancelled.',
 				"You do not have to specify all parameters, only the ones you wish to change.",
+				'The parameter [p]minmaxsize[/p] is limited to a range of 8-72. Any sizes other than that will be ignored.',
 			    'By default, the main mIRC window is the parent. Dialog name OR a hWnd MUST follow after the [s]owner[/s] style. Enter 0 for the hWnd to make it modeless.<br />
 To get hWnd values, use $window().hwnd, $dialog().hwnd, $chan().hwnd, $chat().hwnd, $get().hwnd, $query().hwnd, $send().hwnd, and $timer().hwnd',
 			),
