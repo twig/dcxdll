2500d15cf20c1fca3ae3e2c9292b60009d46b147
twig <twig@17a06da3-8c20-fa79-550f-e9588e724b77>
2006-09-02 04:48:39 +0000

    - added a few more releases to the archive - added filesize to archive information
    git-svn-id: http://dcx.googlecode.com/svn@306 17a06da3-8c20-fa79-550f-e9588e724b77

--- docs_new/download.php
+++ docs_new/download.php
@@ -2,7 +2,10 @@
 header("Cache-Control: no-cache, must-revalidate");	// HTTP/1.1
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");	// Date in the past
 
+// latest stable version
 $stable = '1.3.5';
+
+// archive directory
 $archive = 'archive';
 
 // version => date released
@@ -13,6 +16,12 @@ $versions = array(
 	'1.3.2' => 1149180480,
 	'1.3.1' => 1147423920,
 	'1.3.0' => 1145517420,
+	'1.2.9' => 0,
+	'1.2.8' => 0,
+	'1.2.6' => 0,
+	'1.2.5' => 0,
+	'1.2.3' => 0,
+	'1.2.1' => 0,
 	'1.1.5' => 0,
 );
 
@@ -28,6 +37,14 @@ if ($version) {
 	exit();
 }
 
+function format_filesize($size) {
+	if     ($size /1000000000 >= 1) { return round($size /1073741824, 2) . 'gb'; }
+	elseif ($size /1000000    >= 1) { return round($size /1048576   , 2) . 'mb'; }
+	elseif ($size /1000       >= 1) { return round($size /1024      , 2) . 'kb'; }
+	else                            { return $size . 'b'; }
+}
+
+
 echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">";
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -42,41 +59,47 @@ echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?".">";
 <body>
 
 <table>
-<tr><th>Latest Stable</th><th>Release Date</th><th>Link</th><th>Change Log</th></tr>
+<tr><th>Latest Stable</th><th>Release Date</th><th>Link</th><th>Size</th><th>Change Log</th></tr>
 <?
 // show link for latest stable release
 echo "<tr>
 <td><strong>v$stable</strong></td>
-<td>" . ($versions[$stable] ? date("jS F, Y", $versions[$stable]) : 'Unknown') . "</td>
-<td>";
+<td>" . ($versions[$stable] ? date("jS F, Y", $versions[$stable]) : 'Unknown') . "</td>";
 
-if (file_exists("$archive/dcx_v$stable.zip"))
-	echo "<a href=\"{$_SERVER['PHP_SELF']}?v=$stable\">Download</a>";
-else
-    echo "(Missing)";
+	$file = "$archive/dcx_v$stable.zip";
 
-echo "</td>
-<td><a href=\"changes.htm#v$stable\">Changes</a></td>
+	if (file_exists($file)) {
+		echo "<td><a href=\"{$_SERVER['PHP_SELF']}?v=$version\">Download</a></td>";
+		echo "<td>" . format_filesize(filesize($file)) . "</td>";
+	}
+	else {
+	    echo "<td>(Missing)</td><td>&nbsp;</td>";
+	}
+
+echo "<td><a href=\"changes.htm#v$stable\">Changes</a></td>
 </tr>";
 
 echo "<tr><td colspan='4'>&nbsp;</td></tr>";
 
 // list all download versions
-echo "<tr><th>Version</th><th>Release Date</th><th>Link</th><th>Change Log</th></tr>";
+echo "<tr><th>Version</th><th>Release Date</th><th>Link</th><th>Size</th><th>Change Log</th></tr>";
 
 foreach ($versions as $version => $datereleased) {
 	echo "<tr>
 	<td><strong>v$version</strong></td>
-	<td>" . ($datereleased ? date("jS F, Y", $datereleased) : 'Unknown') . "</td>
- 	<td>";
+	<td>" . ($datereleased ? date("jS F, Y", $datereleased) : 'Unknown') . "</td>";
 
-	if (file_exists("$archive/dcx_v$version.zip"))
-		echo "<a href=\"{$_SERVER['PHP_SELF']}?v=$version\">Download</a>";
-	else
-	    echo "(Missing)";
+	$file = "$archive/dcx_v$version.zip";
 
-	echo "</td>
-	<td><a href=\"changes.htm#v$version\">Changes</a></td>
+	if (file_exists($file)) {
+		echo "<td><a href=\"{$_SERVER['PHP_SELF']}?v=$version\">Download</a></td>";
+		echo "<td>" . format_filesize(filesize($file)) . "</td>";
+	}
+	else {
+	    echo "<td>(Missing)</td><td>&nbsp;</td>";
+	}
+
+	echo "<td><a href=\"changes.htm#v$version\">Changes</a></td>
 	</tr>";
 }
 
