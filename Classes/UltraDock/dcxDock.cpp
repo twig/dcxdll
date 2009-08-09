d1a5151fac50ade868434cd14c41fef24ea3a4cb
ook <ook@17a06da3-8c20-fa79-550f-e9588e724b77>
2009-08-09 18:41:14 +0000

    [xtreebar] fixed crash when trying to show tooltips, bug introduced when function was changed to Dcx::mIRC.execex() forgot to mention this in previous commit :/
    git-svn-id: http://dcx.googlecode.com/svn@1006 17a06da3-8c20-fa79-550f-e9588e724b77

--- Classes/UltraDock/dcxDock.cpp
+++ Classes/UltraDock/dcxDock.cpp
@@ -354,7 +354,7 @@ LRESULT CALLBACK DcxDock::mIRCRefWinProc(HWND mHwnd, UINT uMsg, WPARAM wParam, L
 					//return ((GetWindowLong(mHwnd, GWL_EXSTYLE) & WS_EX_TRANSPARENT) ? TRUE : FALSE);
 			}
 			break;
-#ifndef NDEBUG
+#if DCX_DEBUG_OUTPUT
 		case TVM_SETITEM:
 			{
 				if ( pthis->m_iType != DOCK_TYPE_TREE || !DcxDock::g_bTakeOverTreebar)
@@ -531,7 +531,7 @@ LRESULT CALLBACK DcxDock::mIRCDockWinProc(HWND mHwnd, UINT uMsg, WPARAM wParam, 
 								item.mask = TVIF_TEXT;
 								if (TreeView_GetItem(Dcx::mIRC.getTreeview(), &item)) {
 									DcxDock::getTreebarItemType(tsType, item.lParam);
-									Dcx::mIRC.execex("/!set -nu1 %%dcx_%d %800s", item.lParam, item.pszText );
+									Dcx::mIRC.execex("/!set -nu1 %%dcx_%d %800s", item.lParam, item.pszText ); // <- had wrong args causing instant crash when showing tooltips
 									Dcx::mIRC.tsEvalex(buf, "$xtreebar_callback(gettooltip,%s,%%dcx_%d)", tsType.to_chr(), item.lParam);
 
 									if (buf.len() > 0)
