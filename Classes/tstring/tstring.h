397cecb9e925644e65dbf39203c0294375c600b4
OokEek <aqss45@dsl.pipex.com>
2013-12-21 14:16:41 +0000

    [general] added dcx.props file needed by vs2012/vs2013
    [tstring]
    added some ts_* defines to be used instead of the string functions directly, this allows switching between unicode & multibyte compiles without lots of extra code etc..
    added buffer size tracking to allow us to better manage the memory allocations, this lead to lots of other changes to accommodate this.
    added 2 buffer allocation functions to be called instead of new, these functions insure the buffer size is a multiple of 16bytes. This size was chosen as it allows the use of some sse2+ functions that can read strlen()+16 bytes in the worst case.
    renamed m_pWString as m_pTempString to make its usage clearer.
    added size() function to return the buffersize in use.
    added mreplace() function to replace all chars within a string that match any char in a supplied string with a supplied char.
    changed findtok() to return a size_t

--- Classes/tstring/tstring.h
+++ Classes/tstring/tstring.h
@@ -6,7 +6,7 @@
  * comparisons and token manipulations as done in the mIRC scripting language.
  *
  * \author David Legault ( clickhere at scriptsdb dot org )
- * \version 1.10
+ * \version 1.12
  *
  * \b Revisions
  *	1.1
@@ -37,8 +37,14 @@
  *		Fixed a bug in charToWchar() that caused the conversion to always fail.
  *	1.11
  *		Added strip() function.
+ *	1.12
+ *		changed gettok() to not copy the contents to token first.
+ *		changed findtok() to take an unsigned int arg.
+ *		changed deltok() to take an unsigned int arg.
+ *		changed instok() to take an unsigned arg.
+ *		and loads more...
  *
- * © ScriptsDB.org - 2005
+ * © ScriptsDB.org - 2013
  */
 #if _MSC_VER > 1000
 #pragma once
@@ -63,6 +69,40 @@
 #pragma warning( disable : 2287 )
 #endif
 
+#ifdef UNICODE
+#define ts_upr(x,y) _wcsupr_s((x),(y))
+#define ts_strstr(x,y) wcsstr((x),(y))
+#define ts_strcat_s(x,y,z) wcscat_s((x),(z),(y))
+#define ts_strcat(x,y) lstrcat((x),(y))
+#define ts_strncat(x,y,z) wcsncat((x),(y),(z))
+#define ts_toupper(c) ((((c) >= TEXT('a')) && ((c) <= TEXT('z'))) ? _toupper((c)) : (c) )
+#define ts_strlen(x) lstrlen((x))
+#define ts_strcpyn(dest,src,len) lstrcpyn((dest),(src),(len))
+#define ts_strcpy(dest,src) lstrcpy((dest),(src))
+#define ts_strcmp(x,y) lstrcmp((x),(y))
+#define ts_strncmp(x,y,z) wcsncmp((x),(y),(z))
+#define ts_vscprintf(fmt, args) _vscwprintf((fmt), (args))
+#define ts_vsprintf(txt, cnt, fmt, args ) vswprintf((txt), (cnt), (fmt), (args))
+#else
+#define ts_upr(x,y) _strupr_s((x),(y))
+#define ts_strstr(x,y) strstr((x),(y))
+#define ts_strcat_s(x,y,z) strcat_s((x),(z),(y))
+#define ts_strcat(x,y) lstrcat((x),(y))
+#define ts_strncat(x,y,z) strncat((x),(y),(z))
+//#define ts_toupper(c) ( (((c) >= TEXT('a')) && ((c) <= TEXT('z'))) ? _toupper((c)) : (c) )
+#define ts_toupper(c) rfc_toupper((c))
+#define ts_tolower(c) rfc_tolower((c))
+#define ts_strlen(x) lstrlen((x))
+#define ts_strcpyn(dest,src,len) lstrcpyn((dest),(src),(len))
+#define ts_strcpy(dest,src) lstrcpy((dest),(src))
+#define ts_strcmp(x,y) lstrcmp((x),(y))
+#define ts_strncmp(x,y,z) strncmp((x),(y),(z))
+#define ts_vscprintf(fmt, args) _vscprintf((fmt), (args))
+#define ts_vsprintf(txt, cnt, fmt, args ) vsprintf_s((txt), (cnt), (fmt), (args))
+#endif
+#define TS_getmemsize(x) ((x) + (16 - ((x) % 16)))
+#define TS_wgetmemsize(x) (unsigned long)((((x)*sizeof(WCHAR)) + (16 - (((x)*sizeof(WCHAR)) % 16)))/sizeof(WCHAR))
+
 /*!
  * \brief String and Token Management Class
  */
@@ -70,41 +110,50 @@ class TString {
 
 private:
 
-	void deleteString( );
-	void deleteWString( );
+	void deleteString( const bool bKeepBufferSize = false );
+	void deleteWString(const bool bKeepBufferSize = false);
 	int i_replace(const TCHAR *subString, const TCHAR *rString); // Ook
 	int i_remove(const TCHAR *subString);
 	static int match (register TCHAR *m, register TCHAR *n, const bool cs /* case sensitive */);
+
+	size_t set_buffersize(const size_t size) { m_buffersize = TS_getmemsize(size); return m_buffersize; };	// make buffersize a multiple of 16
+	//allocate a buffer thats sized to multiples of 16 bytes, (%byte + (16 - (%byte % 16)))
+	TCHAR *allocstr_bytes(const size_t size) { return (TCHAR *)(new BYTE[set_buffersize(size)]); };
+	// allocate a buffer thats size characters long & its a multiple of 16bytes.
+	TCHAR *allocstr_cch(const size_t size) { return allocstr_bytes(size*sizeof(TCHAR)); };
+	//TCHAR *allocstr_cch(const size_t size) { return (TCHAR *)(new BYTE[set_buffersize(size*sizeof(TCHAR))]); };
+
 #if !UNICODE
-	static unsigned TCHAR tolowertab[];
-	static unsigned TCHAR touppertab[];
+	static unsigned char tolowertab[];
+	static unsigned char touppertab[];
 #endif
-	static WCHAR *charToWchar(const char *cString);
-	static char *WcharTochar(const WCHAR *wString);
+	static WCHAR *charToWchar(const char *cString, size_t *buffer_size = NULL);
+	static char *WcharTochar(const WCHAR *wString, size_t *buffer_size = NULL);
 
 	mutable TCHAR			*m_savedpos;
 	mutable unsigned int	m_savedtotaltoks;
 	mutable unsigned int	m_savedcurrenttok;
+	size_t					m_buffersize;	// size of string buffer in use. (size in bytes)
 
 public:
 
 	TCHAR * m_pString; //!< String buffer
 #ifdef UNICODE
-	mutable char * m_pWString;
+	mutable char * m_pTempString;
 #else
-	mutable WCHAR * m_pWString;
+	mutable WCHAR * m_pTempString;
 #endif
 	static const TCHAR *m_cSpace;
 	static const TCHAR *m_cComma;
 	static const TCHAR *m_cTab;
 
 	TString( );
-	TString( const WCHAR * cString ); // we don't want these 2 as explicits
+	TString( const WCHAR * cString ); // we don't want these 3 as explicits
 	TString( const char * cString );
-	explicit TString( const WCHAR chr );
+	TString( const TString & tString);
+	TString( const TCHAR *pStart, const TCHAR *pEnd);
+	explicit TString( const WCHAR chr);
 	explicit TString( const char chr );
-	TString( const TString & tString ); // or this one.
-	TString( const TCHAR *pStart, const TCHAR *pEnd );
 	explicit TString( const unsigned int tsSize);
 
 	//! Destructor
@@ -169,6 +218,7 @@ public:
 
 	// General String Lib
 	size_t len( ) const;
+	size_t size() const { return m_buffersize; };
 
 	int find( const TCHAR * substring, const int N ) const;
 	int find( const TCHAR chr, const int N ) const;
@@ -182,13 +232,14 @@ public:
 	int replace( const TCHAR * subString, const TCHAR rchr );
 	int replace( const TCHAR chr, const TCHAR * rString );
 	int replace( const TCHAR chr, const TCHAR rchr );
+	int mreplace(const TCHAR chr, const TCHAR *rchr);
 
 	// Token Lib
 	void addtok( const TCHAR * cToken, const TCHAR * sepChars = SPACE );
 	void addtok( const __int64 nToken, const TCHAR * sepChars = SPACE );
 	//void addtok( const TString &cToken, const TCHAR * sepChars = SPACE );
 	void deltok( const unsigned int N, const TCHAR * sepChars = SPACE );
-	unsigned int findtok( const TCHAR * cToken, const unsigned int N, const TCHAR * sepChars = SPACE ) const;
+	size_t findtok( const TCHAR * cToken, const unsigned int N, const TCHAR * sepChars = SPACE ) const;
 	TString gettok( int N, const TCHAR * sepChars = SPACE ) const;
 	TString gettok( int N, int M, const TCHAR * sepChars = SPACE ) const;
 	TString getfirsttok( const unsigned int N, const TCHAR * sepChars = SPACE ) const;	// must becalled before the first getnexttok()
@@ -204,22 +255,24 @@ public:
 	// added by Ook
 	TString wildtok( TCHAR * wildString, int N, const TCHAR * sepChars = SPACE ) const;
 	int nwildtok( TCHAR * wildString, const TCHAR * sepChars = SPACE ) const;
+
+	// extract left/right/mid
 	TString mid(const int pos, int n) const;
 	TString left(int n) const;
 	TString right(int n) const;
+
 	int tsprintf(const TCHAR *fmt, ...);
 	int tvprintf(const TCHAR *fmt, va_list * args);
 	bool iswm(TCHAR *a) const;
 	bool iswmcs(TCHAR *a) const;
+	TCHAR * to_chr() const { return this->m_pString; };	// returns the string in the projects current format.
 #ifdef UNICODE
-	TCHAR * to_chr( ) const { return this->m_pString; };	// returns the string in the projects current format.
 	WCHAR *to_wchr(bool tryutf8 = false) const { return this->m_pString; };	// returns the string in wide format
 	char * c_str(void) const;	// returns the string as a char *
 	int to_int() const { return _wtoi(this->m_pString); };
 	__int64 to_num( ) const { return _wtoi64(this->m_pString); };
 	double to_float() const { return _wtof(this->m_pString); };
 #else
-	TCHAR * to_chr( ) const { return this->m_pString; };	// returns the string in the projects current format.
 	WCHAR *to_wchr(bool tryutf8 = false);	// returns the string in wide format
 	char * c_str( ) const { return this->m_pString; };	// returns the string as a char *
 	int to_int() const { return atoi(this->m_pString); };
