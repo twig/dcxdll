74685f6fcb682bdb5ba75c2e7005675a8296634f
OokEek <aqss45@dsl.pipex.com>
2015-12-27 03:10:42 +0000

    [general] bumped the versions

--- defines.h
+++ defines.h
@@ -151,7 +151,7 @@ http://symbiancorner.blogspot.com/2007/05/how-to-detect-version-of-ms-visual.htm
 // --------------------------------------------------
 #define DLL_VERSION    GIT_DESCRIBE
 #define DLL_BUILD      GIT_HASH
-#define DLL_DEV_BUILD  38
+#define DLL_DEV_BUILD  39
 
 #ifdef NDEBUG
 #ifdef DCX_DEV_BUILD
