c92c9b8bd1638f006772e652c239c29d2d36a307
OokEek <aqss45@dsl.pipex.com>
2021-11-19 22:58:12 +0000

    [link] fixed the fix...

--- Classes/mirc/dcxlink.cpp
+++ Classes/mirc/dcxlink.cpp
@@ -189,7 +189,7 @@ void DcxLink::parseCommandRequest(const TString& input)
 
 		const auto tsArgs(input.getlasttoks());			// tok 4, -1
 
-		if (tsArgs.numtok() >= std::size(m_aColors))
+		if (tsArgs.numtok() > std::size(m_aColors))
 			throw DcxExceptions::dcxInvalidArguments();
 
 		UINT i = 0U;
