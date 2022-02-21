d574177e5cd3d1b9f3615321398a49731ef33cd7
OokEek <aqss45@dsl.pipex.com>
2022-02-21 11:33:12 +0000

    [general] added constructor for XSwitchFlags

--- XSwitchFlags.cpp
+++ XSwitchFlags.cpp
@@ -1,9 +1,44 @@
 #include "defines.h"
 
 // Constructor
+//XSwitchFlags::XSwitchFlags(const TString& switches) noexcept
+//	: m_dFlagMask(0ULL)
+//{
+//	// no - sign, & no + sign, invalid syntax
+//	if (switches[0] == TEXT('-'))
+//		m_dFlagMask |= (1ULL << 60);
+//	else if (switches[0] == TEXT('+'))
+//		m_dFlagMask |= (1ULL << 61);
+//	else
+//		return;	// just return with no flags set.
+//
+//	const auto len = switches.len();
+//
+//	// Parse the switches
+//	for (auto i = decltype(len){1}; i < len; ++i)
+//	{
+//		const auto c = switches[i];
+//
+//		// Check if its in the right range
+//		if ((c >= TEXT('A')) && (c <= TEXT('z'))) // this gives us 57bits in use + 2 special cases
+//		{
+//			const auto bitn = c - TEXT('A');
+//			m_dFlagMask |= (1ULL << bitn);
+//		}
+//	}
+//}
+
 XSwitchFlags::XSwitchFlags(const TString& switches) noexcept
+	: XSwitchFlags(switches.to_chr(), switches.len())
+{
+}
+
+XSwitchFlags::XSwitchFlags(const TCHAR* const switches, size_t len) noexcept
 	: m_dFlagMask(0ULL)
 {
+	if (!switches || !len)
+		return;
+
 	// no - sign, & no + sign, invalid syntax
 	if (switches[0] == TEXT('-'))
 		m_dFlagMask |= (1ULL << 60);
@@ -11,8 +46,6 @@ XSwitchFlags::XSwitchFlags(const TString& switches) noexcept
 		m_dFlagMask |= (1ULL << 61);
 	else
 		return;	// just return with no flags set.
-
-	const auto len = switches.len();
 
 	// Parse the switches
 	for (auto i = decltype(len){1}; i < len; ++i)
--- XSwitchFlags.h
+++ XSwitchFlags.h
@@ -8,7 +8,7 @@
  * Adding a control specific switch will override the global command.
  * It is important to update this when adding new global commands.
  *
- * a = 0		
+ * a = 0
  * b = 1		xdid
  * c = 2				XDID
  * d = 3
@@ -41,19 +41,91 @@
 
 #include "Classes/TString/tstring.h"
 
+enum class XSwitchValues
+	: ULONGLONG
+{
+	xA = (1ULL << ('A' - 'A')), // 0x0000000000000001
+	xB = (1ULL << ('B' - 'A')), // 0x0000000000000002
+	xC = (1ULL << ('C' - 'A')), // 0x0000000000000004
+	xD = (1ULL << ('D' - 'A')), // 0x0000000000000008
+	xE = (1ULL << ('E' - 'A')), // 0x0000000000000010
+	xF = (1ULL << ('F' - 'A')), // 0x0000000000000020
+	xG = (1ULL << ('G' - 'A')), // 0x0000000000000040
+	xH = (1ULL << ('H' - 'A')), // 0x0000000000000080
+	xI = (1ULL << ('I' - 'A')), // 0x0000000000000100
+	xJ = (1ULL << ('J' - 'A')), // 0x0000000000000200
+	xK = (1ULL << ('K' - 'A')), // 0x0000000000000400
+	xL = (1ULL << ('L' - 'A')), // 0x0000000000000800
+	xM = (1ULL << ('M' - 'A')), // 0x0000000000001000
+	xN = (1ULL << ('N' - 'A')), // 0x0000000000002000
+	xO = (1ULL << ('O' - 'A')), // 0x0000000000004000
+	xP = (1ULL << ('P' - 'A')), // 0x0000000000008000
+	xQ = (1ULL << ('Q' - 'A')), // 0x0000000000010000
+	xR = (1ULL << ('R' - 'A')), // 0x0000000000020000
+	xS = (1ULL << ('S' - 'A')), // 0x0000000000040000
+	xT = (1ULL << ('T' - 'A')), // 0x0000000000080000
+	xU = (1ULL << ('U' - 'A')), // 0x0000000000100000
+	xV = (1ULL << ('V' - 'A')), // 0x0000000000200000
+	xW = (1ULL << ('W' - 'A')), // 0x0000000000400000
+	xX = (1ULL << ('X' - 'A')), // 0x0000000000800000
+	xY = (1ULL << ('Y' - 'A')), // 0x0000000001000000
+	xZ = (1ULL << ('Z' - 'A')), // 0x0000000002000000
+	// 26 [ 0x0000000004000000
+	// 27 | 0x0000000008000000
+	// 28 ] 0x0000000010000000
+	// 29 ^ 0x0000000020000000
+	// 30 _ 0x0000000040000000
+	// 31 ' 0x0000000080000000
+	xa = (1ULL << ('a' - 'A')), // 0x0000000100000000
+	xb = (1ULL << ('b' - 'A')), // 0x0000000200000000
+	xc = (1ULL << ('c' - 'A')), // 0x0000000400000000
+	xd = (1ULL << ('d' - 'A')), // 0x0000000800000000
+	xe = (1ULL << ('e' - 'A')), // 0x0000001000000000
+	xf = (1ULL << ('f' - 'A')), // 0x0000002000000000
+	xg = (1ULL << ('g' - 'A')), // 0x0000004000000000
+	xh = (1ULL << ('h' - 'A')), // 0x0000008000000000
+	xi = (1ULL << ('i' - 'A')), // 0x0000010000000000
+	xj = (1ULL << ('j' - 'A')), // 0x0000020000000000
+	xk = (1ULL << ('k' - 'A')), // 0x0000040000000000
+	xl = (1ULL << ('l' - 'A')), // 0x0000080000000000
+	xm = (1ULL << ('m' - 'A')), // 0x0000100000000000
+	xn = (1ULL << ('n' - 'A')), // 0x0000200000000000
+	xo = (1ULL << ('o' - 'A')), // 0x0000400000000000
+	xp = (1ULL << ('p' - 'A')), // 0x0000800000000000
+	xq = (1ULL << ('q' - 'A')), // 0x0001000000000000
+	xr = (1ULL << ('r' - 'A')), // 0x0002000000000000
+	xs = (1ULL << ('s' - 'A')), // 0x0004000000000000
+	xt = (1ULL << ('t' - 'A')), // 0x0008000000000000
+	xu = (1ULL << ('u' - 'A')), // 0x0010000000000000
+	xv = (1ULL << ('v' - 'A')), // 0x0020000000000000
+	xw = (1ULL << ('w' - 'A')), // 0x0040000000000000
+	xx = (1ULL << ('x' - 'A')), // 0x0080000000000000
+	xy = (1ULL << ('y' - 'A')), // 0x0100000000000000
+	xz = (1ULL << ('z' - 'A')), // 0x0200000000000000
+	// 58 empty					   0x0400000000000000
+	// 59 empty					   0x0800000000000000
+	xMinus = (1ULL << 60),		// 0x1000000000000000
+	xPlus = (1ULL << 61)		// 0x2000000000000000
+	// 62 empty					   0x4000000000000000
+	// 63 empty					   0x8000000000000000
+};
 struct XSwitchFlags final
 {
 	XSwitchFlags() = delete;
-	XSwitchFlags(const XSwitchFlags &) = delete;
-	XSwitchFlags &operator =(const XSwitchFlags &) = delete;
-	XSwitchFlags(XSwitchFlags &&) = delete;
-	XSwitchFlags &operator =(XSwitchFlags &&) = delete;
+	XSwitchFlags(const XSwitchFlags&) = delete;
+	XSwitchFlags& operator =(const XSwitchFlags&) = delete;
+	XSwitchFlags(XSwitchFlags&&) = delete;
+	XSwitchFlags& operator =(XSwitchFlags&&) = delete;
 
-	explicit XSwitchFlags(const TString &switches) noexcept;
+	explicit XSwitchFlags(const TString& switches) noexcept;
+	XSwitchFlags(const TCHAR* const switches, size_t len) noexcept;
 	//~XSwitchFlags() = default;
 
 	// Function checks if flag is set
 	const bool isSet(const TCHAR c) const noexcept;
+	// returns the whole mask
+	const ULONGLONG& getMask() const noexcept { return m_dFlagMask; }
+
 	const bool operator[](const TCHAR c) const noexcept { return isSet(c); }
 	explicit operator ULONGLONG() const noexcept { return m_dFlagMask; }
 
