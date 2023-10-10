69d60d90a7a7fb40e1d6b4c685909b649c25b41d
OokEek <aqss45@dsl.pipex.com>
2023-10-10 11:48:56 +0100

    [detours] added a copy of the detours compiled files to this project as people seem to be having issues with compiling it. changed the dcx.props file to point at the local copys.
    [docs]
    updated the how to compile section for the above chan ges.
    
    [pcre2]
    updated to the latest commits.

--- docs_new/inc/howtocompile.php
+++ docs_new/inc/howtocompile.php
@@ -16,11 +16,11 @@ function get_intro_howtocompile() {
 	<tr style="text-indent: 40px"><td>Full latest windows desktop SDK installed.</td></tr>
     <tr style="text-indent: 40px"><td>DCX Source. <a href='https://github.com/twig/dcxdll'>https://github.com/twig/dcxdll</a></td></tr>
     <tr style="text-indent: 40px"><td>GSL installed. <a href='https://github.com/microsoft/GSL.git'>https://github.com/microsoft/GSL.git</a></td></tr>
-	<tr style="text-indent: 40px"><td>Detours installed. <a href='https://github.com/microsoft/Detours.git'>https://github.com/microsoft/Detours.git</a></td></tr>
 	<tr style="text-indent: 40px"><td>PHP v7+ installed, recommended PHP nts 8.2.11+ (only for the docs) <a href='https://www.php.net'>https://www.php.net/</a></td></tr>
-	<tr style="text-indent: 40px"><td>Git for windows installed.</td></tr>
+	<tr style="text-indent: 40px"><td>Git for windows installed. (This is needed to auto create gitBuild.h)</td></tr>
 	<tr style="text-indent: 40px"><td>mIRC v7+ installed, recommended v7.75+. (only for debugging) <a href='https://www.mirc.co.uk/'>https://www.mirc.co.uk/</a></td></tr>
 	<tr><td><strong>Optional:</strong></td></tr>
+	<tr style="text-indent: 40px"><td>Detours installed. Only if you don't wish to use the supplied libs. <a href='https://github.com/microsoft/Detours.git'>https://github.com/microsoft/Detours.git</a></td></tr>
 	<tr style="text-indent: 40px"><td>PCRE2 installed. Only if you don't wish to use the supplied libs. <a href='https://github.com/PhilipHazel/pcre2.git'>https://github.com/PhilipHazel/pcre2.git</a></td></tr>
 	</table>
 <?php
@@ -46,8 +46,8 @@ Near the top you will see <code>&lt;PropertyGroup Label="UserMacros"&gt;</code> 
 	  <td class="desc">Path to the php.exe</td>
 	</tr>
     <tr>
-	  <td class="tag"><code>&lt;DetoursDir&gt;$(SolutionDir)..\..\DetoursMain\&lt;/DetoursDir&gt;</code></td>
-	  <td class="desc">Path to the Detours files.</td>
+	  <td class="tag"><code>&lt;DetoursDir&gt;$(SolutionDir)Detours\&lt;/DetoursDir&gt;</code></td>
+	  <td class="desc">Path to the Detours files. Only change this if you decide to compile Detours yourself.</td>
 	</tr>
     <tr>
 	  <td class="tag"><code>&lt;GSLDir&gt;F:\CSource\GSL-Master\GSL\include&lt;/GSLDir&gt;</code></td>
@@ -63,9 +63,12 @@ Near the top you will see <code>&lt;PropertyGroup Label="UserMacros"&gt;</code> 
 	</tr>
 </table>
 <br />
-These entries must be changed to match the system you are using.
-<br />
-After these have been edited you can load the solution file and compile.
+These entries must be changed to match the system you are using.<br />
+Before compiling DCX you must first have Detours compiled, You will need the 32bit debug & 32bit release builds.
+After these have been edited you can load the solution file and compile.<br />
+When compiling you should see the "SourceControl" project run before the DCX project, this creates gitBuild.h<br />
+You should also see the project "DxErr" run before DCX, this creates the DxErr.lib file needed by DCX.<br />
+
 <br />
 <?php
 	return ob_get_clean();
