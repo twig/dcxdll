b9258264a526760e83affcc72fdf97ed8a4d7a33
OokEek <aqss45@dsl.pipex.com>
2024-01-23 22:13:02 +0000

    [dpimodule] added more functions.

--- DcxDPIModule.cpp
+++ DcxDPIModule.cpp
@@ -26,16 +26,38 @@ bool DcxDPIModule::load()
 #pragma warning(disable: 4191)
 #pragma warning(disable: 26493)	//warning C26493 : Don't use C-style casts that would perform a static_cast downcast, const_cast, or reinterpret_cast. (type.4: http://go.microsoft.com/fwlink/p/?LinkID=620420)
 
-		GetDpiForSystemUx = reinterpret_cast<decltype(::GetDpiForSystem)*>(GetProcAddress(m_hModule, "GetDpiForSystem"));
-		GetDpiForWindowUx = reinterpret_cast<decltype(::GetDpiForWindow)*>(GetProcAddress(m_hModule, "GetDpiForWindow"));
-		GetSystemMetricsForDpiUx = reinterpret_cast<decltype(::GetSystemMetricsForDpi)*>(GetProcAddress(m_hModule, "GetSystemMetricsForDpi"));
-		SetProcessDpiAwarenessContextUx = reinterpret_cast<decltype(::SetProcessDpiAwarenessContext)*>(::GetProcAddress(m_hModule, "SetProcessDpiAwarenessContext"));
-		InheritWindowMonitorUx = reinterpret_cast<decltype(::InheritWindowMonitor)*>(::GetProcAddress(m_hModule, "InheritWindowMonitor"));
-		SetThreadDpiHostingBehaviorUx = reinterpret_cast<decltype(::SetThreadDpiHostingBehavior)*>(::GetProcAddress(m_hModule, "SetThreadDpiHostingBehavior"));
+#define STRINGIFY(proc) #proc
+#define ASSIGN_PROC(proc) proc ## Ux = reinterpret_cast<decltype(::proc)*>(GetProcAddress(m_hModule, STRINGIFY(proc)));
+
+		ASSIGN_PROC(AdjustWindowRectExForDpi);
+		ASSIGN_PROC(AreDpiAwarenessContextsEqual);
+		ASSIGN_PROC(EnableNonClientDpiScaling);
+		ASSIGN_PROC(GetAwarenessFromDpiAwarenessContext);
+		ASSIGN_PROC(GetDialogControlDpiChangeBehavior);
+		ASSIGN_PROC(GetDialogDpiChangeBehavior);
+		ASSIGN_PROC(GetDpiAwarenessContextForProcess);
+		ASSIGN_PROC(GetDpiForSystem);
+		ASSIGN_PROC(GetDpiForWindow);
+		ASSIGN_PROC(GetDpiFromDpiAwarenessContext);
+		ASSIGN_PROC(GetSystemDpiForProcess);
+		ASSIGN_PROC(GetSystemMetricsForDpi);
+		ASSIGN_PROC(GetThreadDpiAwarenessContext);
+		ASSIGN_PROC(GetThreadDpiHostingBehavior);
+		ASSIGN_PROC(GetWindowDpiAwarenessContext);
+		ASSIGN_PROC(GetWindowDpiHostingBehavior);
+		ASSIGN_PROC(InheritWindowMonitor);
+		ASSIGN_PROC(IsValidDpiAwarenessContext);
+		ASSIGN_PROC(SetDialogControlDpiChangeBehavior);
+		ASSIGN_PROC(SetDialogDpiChangeBehavior);
+		ASSIGN_PROC(SetProcessDpiAwarenessContext);
+		ASSIGN_PROC(SetThreadCursorCreationScaling);
+		ASSIGN_PROC(SetThreadDpiAwarenessContext);
+		ASSIGN_PROC(SetThreadDpiHostingBehavior);
+		ASSIGN_PROC(SystemParametersInfoForDpi);
 
 #pragma warning(pop)
 
-		if (GetDpiForSystemUx && GetDpiForWindowUx && GetSystemMetricsForDpiUx && InheritWindowMonitorUx && SetThreadDpiHostingBehaviorUx)
+		if (GetDpiForSystemUx && GetDpiForWindowUx && GetSystemMetricsForDpiUx)
 		{
 			DCX_DEBUG(mIRCLinker::debug, __FUNCTIONW__, TEXT("Found Win10+ DPI Functions"));
 		}
@@ -59,22 +81,135 @@ bool DcxDPIModule::unload() noexcept
 		m_hModule = nullptr;
 	}
 	// make sure all functions are nullptr
+	AdjustWindowRectExForDpiUx = nullptr;
+	AreDpiAwarenessContextsEqualUx = nullptr;
+	EnableNonClientDpiScalingUx = nullptr;
+	GetAwarenessFromDpiAwarenessContextUx = nullptr;
+	GetDialogControlDpiChangeBehaviorUx = nullptr;
+	GetDialogDpiChangeBehaviorUx = nullptr;
+	GetDpiAwarenessContextForProcessUx = nullptr;
 	GetDpiForSystemUx = nullptr;
 	GetDpiForWindowUx = nullptr;
+	GetDpiFromDpiAwarenessContextUx = nullptr;
+	GetSystemDpiForProcessUx = nullptr;
 	GetSystemMetricsForDpiUx = nullptr;
-	SetProcessDpiAwarenessContextUx = nullptr;
+	GetThreadDpiAwarenessContextUx = nullptr;
+	GetThreadDpiHostingBehaviorUx = nullptr;
+	GetWindowDpiAwarenessContextUx = nullptr;
+	GetWindowDpiHostingBehaviorUx = nullptr;
 	InheritWindowMonitorUx = nullptr;
+	IsValidDpiAwarenessContextUx = nullptr;
+	SetDialogControlDpiChangeBehaviorUx = nullptr;
+	SetDialogDpiChangeBehaviorUx = nullptr;
+	SetProcessDpiAwarenessContextUx = nullptr;
+	SetThreadCursorCreationScalingUx = nullptr;
+	SetThreadDpiAwarenessContextUx = nullptr;
 	SetThreadDpiHostingBehaviorUx = nullptr;
+	SystemParametersInfoForDpiUx = nullptr;
 
 	return isUseable();
 }
 #pragma warning(pop)
 
-/*!
-* \brief Get systems DPI
-*
-*
-*/
+/// <summary>
+/// Does the same as AdjustWindowRectEx() but taking a given dpi into account.
+/// </summary>
+/// <param name="lpRect"></param>
+/// <param name="dwStyle"></param>
+/// <param name="bMenu"></param>
+/// <param name="dwExStyle"></param>
+/// <param name="dpi"></param>
+/// <returns></returns>
+BOOL DcxDPIModule::dcxAdjustWindowRectExForDpi(_Inout_ LPRECT lpRect, _In_ DWORD dwStyle, _In_ BOOL bMenu, _In_ DWORD dwExStyle, _In_ UINT dpi) noexcept
+{
+	if (AdjustWindowRectExForDpiUx)
+		return AdjustWindowRectExForDpiUx(lpRect, dwStyle, bMenu, dwExStyle, dpi);
+	return AdjustWindowRectEx(lpRect, dwStyle, bMenu, dwExStyle);
+}
+
+/// <summary>
+/// Test if two contexts are the same.
+/// </summary>
+/// <param name="dpiContextA"></param>
+/// <param name="dpiContextB"></param>
+/// <returns></returns>
+BOOL DcxDPIModule::dcxAreDpiAwarenessContextsEqual(_In_ DPI_AWARENESS_CONTEXT dpiContextA, _In_ DPI_AWARENESS_CONTEXT dpiContextB) noexcept
+{
+	if (AreDpiAwarenessContextsEqualUx)
+		return AreDpiAwarenessContextsEqualUx(dpiContextA, dpiContextB);
+	SetLastError(ERROR_INVALID_FUNCTION);
+	return FALSE;
+}
+
+/// <summary>
+/// Enable non client scaling.
+/// </summary>
+/// <param name="hwnd"></param>
+/// <returns></returns>
+BOOL DcxDPIModule::dcxEnableNonClientDpiScaling(_In_ HWND hwnd) noexcept
+{
+	if (EnableNonClientDpiScalingUx)
+		return EnableNonClientDpiScalingUx(hwnd);
+	SetLastError(ERROR_INVALID_FUNCTION);
+	return FALSE;
+}
+
+/// <summary>
+/// Get the awareness value from a given context.
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+DPI_AWARENESS DcxDPIModule::dcxGetAwarenessFromDpiAwarenessContext(_In_ DPI_AWARENESS_CONTEXT value) noexcept
+{
+	if (GetAwarenessFromDpiAwarenessContextUx)
+		return GetAwarenessFromDpiAwarenessContextUx(value);
+	SetLastError(ERROR_INVALID_FUNCTION);
+	return DPI_AWARENESS_INVALID;
+}
+
+/// <summary>
+/// Get the behaviour of a given dialog control when the dpi changes.
+/// </summary>
+/// <param name="hWnd"></param>
+/// <returns></returns>
+DIALOG_CONTROL_DPI_CHANGE_BEHAVIORS DcxDPIModule::dcxGetDialogControlDpiChangeBehavior(HWND hWnd) noexcept
+{
+	if (GetDialogControlDpiChangeBehaviorUx)
+		return GetDialogControlDpiChangeBehaviorUx(hWnd);
+	SetLastError(ERROR_INVALID_FUNCTION);
+	return DCDC_DEFAULT;
+}
+
+/// <summary>
+/// Get the behaviour of a given dialog when the dpi changes.
+/// </summary>
+/// <param name="hDlg"></param>
+/// <returns></returns>
+DIALOG_DPI_CHANGE_BEHAVIORS DcxDPIModule::dcxGetDialogDpiChangeBehavior(HWND hDlg) noexcept
+{
+	if (GetDialogDpiChangeBehaviorUx)
+		return GetDialogDpiChangeBehaviorUx(hDlg);
+	SetLastError(ERROR_INVALID_FUNCTION);
+	return DDC_DEFAULT;
+}
+
+/// <summary>
+/// Get the awareness context for a given process.
+/// </summary>
+/// <param name="hProcess"></param>
+/// <returns></returns>
+DPI_AWARENESS_CONTEXT DcxDPIModule::dcxGetDpiAwarenessContextForProcess(HANDLE hProcess) noexcept
+{
+	if (GetDpiAwarenessContextForProcessUx)
+		return GetDpiAwarenessContextForProcessUx(hProcess);
+	SetLastError(ERROR_INVALID_FUNCTION);
+	return nullptr;
+}
+
+/// <summary>
+/// Get systems DPI
+/// </summary>
+/// <returns></returns>
 UINT DcxDPIModule::dcxGetDpiForSystem() noexcept
 {
 	if (GetDpiForSystemUx)
@@ -82,11 +217,11 @@ UINT DcxDPIModule::dcxGetDpiForSystem() noexcept
 	return 96;
 }
 
-/*!
-* \brief Get DPI for window
-*
-*
-*/
+/// <summary>
+/// Get DPI for window
+/// </summary>
+/// <param name="hwnd"></param>
+/// <returns></returns>
 UINT DcxDPIModule::dcxGetDpiForWindow(_In_ _Maybenull_ HWND hwnd) noexcept
 {
 	if (hwnd && GetDpiForWindowUx)
@@ -94,16 +229,107 @@ UINT DcxDPIModule::dcxGetDpiForWindow(_In_ _Maybenull_ HWND hwnd) noexcept
 	return dcxGetDpiForSystem();
 }
 
-/*!
-* \brief Get system metrics adjusted for specified DPI
-*
-*
-*/
+/// <summary>
+/// Get the dpi value for a given dpi awareness context.
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+UINT DcxDPIModule::dcxGetDpiFromDpiAwarenessContext(_In_ DPI_AWARENESS_CONTEXT value) noexcept
+{
+	if (GetDpiFromDpiAwarenessContextUx)
+		return GetDpiFromDpiAwarenessContextUx(value);
+	SetLastError(ERROR_INVALID_FUNCTION);
+	return 0;
+}
+
+/// <summary>
+/// Get the system dpi setting for a given process.
+/// Just returns 96 if the dpi function is not available.
+/// </summary>
+/// <param name="hProcess"></param>
+/// <returns></returns>
+UINT DcxDPIModule::dcxGetSystemDpiForProcess(HANDLE hProcess) noexcept
+{
+	if (GetSystemDpiForProcessUx)
+		return GetSystemDpiForProcessUx(hProcess);
+	// return default dpi of 100% when no function available.
+	return 96;
+}
+
+/// <summary>
+/// Get system metrics adjusted for specified DPI.
+/// Falls back to GetSystemMetrics() if the dpi equiv is not available.
+/// </summary>
+/// <param name="nIndex"></param>
+/// <param name="dpi"></param>
+/// <returns></returns>
 UINT DcxDPIModule::dcxGetSystemMetricsForDpi(_In_ int nIndex, _In_ UINT dpi) noexcept
 {
 	if (GetSystemMetricsForDpiUx)
 		return GetSystemMetricsForDpiUx(nIndex, dpi);
 	return GetSystemMetrics(nIndex);
+}
+
+/// <summary>
+/// Get the awareness context for this thread.
+/// </summary>
+/// <returns></returns>
+DPI_AWARENESS_CONTEXT DcxDPIModule::dcxGetThreadDpiAwarenessContext() noexcept
+{
+	if (GetThreadDpiAwarenessContextUx)
+		return GetThreadDpiAwarenessContextUx();
+	return nullptr;
+}
+
+/// <summary>
+/// Get the dpi hosting behaviour for this thread.
+/// </summary>
+/// <returns></returns>
+DPI_HOSTING_BEHAVIOR DcxDPIModule::dcxGetThreadDpiHostingBehavior() noexcept
+{
+	if (GetThreadDpiHostingBehaviorUx)
+		return GetThreadDpiHostingBehaviorUx();
+	return DPI_HOSTING_BEHAVIOR_INVALID;
+}
+
+/// <summary>
+/// Get the awareness context for the given window.
+/// </summary>
+/// <param name="hwnd"></param>
+/// <returns></returns>
+DPI_AWARENESS_CONTEXT DcxDPIModule::dcxGetWindowDpiAwarenessContext(_In_ HWND hwnd) noexcept
+{
+	if (GetWindowDpiAwarenessContextUx)
+		return GetWindowDpiAwarenessContextUx(hwnd);
+	return nullptr;
+}
+
+/// <summary>
+/// Get the dpi hosting behaviour for the given window.
+/// </summary>
+/// <param name="hwnd"></param>
+/// <returns></returns>
+DPI_HOSTING_BEHAVIOR DcxDPIModule::dcxGetWindowDpiHostingBehavior(_In_ HWND hwnd) noexcept
+{
+	if (GetWindowDpiHostingBehaviorUx)
+		return GetWindowDpiHostingBehaviorUx(hwnd);
+	return DPI_HOSTING_BEHAVIOR_INVALID;
+}
+
+/// <summary>
+/// Get the system parameter for a given dpi.
+/// </summary>
+/// <param name="uiAction">- The system-wide parameter to be retrieved. This function is only intended for use with SPI_GETICONTITLELOGFONT, SPI_GETICONMETRICS, or SPI_GETNONCLIENTMETRICS</param>
+/// <param name="uiParam"></param>
+/// <param name="pvParam"></param>
+/// <param name="fWinIni"></param>
+/// <param name="dpi">- The DPI to use for scaling the metric.</param>
+/// <returns></returns>
+BOOL DcxDPIModule::dcxSystemParametersInfoForDpi(_In_ UINT uiAction, _In_ UINT uiParam, _Inout_ PVOID pvParam, _In_ UINT fWinIni, _In_ UINT dpi) noexcept
+{
+	if (SystemParametersInfoForDpiUx)
+		return SystemParametersInfoForDpiUx(uiAction, uiParam, pvParam, fWinIni, dpi);
+	return FALSE;
 }
 
 /// <summary>
@@ -118,11 +344,32 @@ UINT DcxDPIModule::dcxGetWindowMetrics(_In_ _Maybenull_ HWND hwnd, _In_ int nInd
 	return dcxGetSystemMetricsForDpi(nIndex, dpi);
 }
 
-BOOL DcxDPIModule::dcxSetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT value) noexcept
+BOOL DcxDPIModule::dcxSetDialogDpiChangeBehavior(HWND hDlg, _In_ DIALOG_DPI_CHANGE_BEHAVIORS mask, _In_ DIALOG_DPI_CHANGE_BEHAVIORS values) noexcept
+{
+	if (SetDialogDpiChangeBehaviorUx)
+		return SetDialogDpiChangeBehaviorUx(hDlg, mask, values);
+	return FALSE;
+}
+
+BOOL DcxDPIModule::dcxSetProcessDpiAwarenessContext(_In_ DPI_AWARENESS_CONTEXT value) noexcept
 {
 	if (SetProcessDpiAwarenessContextUx)
 		return SetProcessDpiAwarenessContextUx(value);
 	return FALSE;
+}
+
+UINT DcxDPIModule::dcxSetThreadCursorCreationScaling(_In_ UINT cursorDpi) noexcept
+{
+	if (SetThreadCursorCreationScalingUx)
+		return SetThreadCursorCreationScalingUx(cursorDpi);
+	return 96;
+}
+
+DPI_AWARENESS_CONTEXT DcxDPIModule::dcxSetThreadDpiAwarenessContext(_In_ DPI_AWARENESS_CONTEXT dpiContext) noexcept
+{
+	if (SetThreadDpiAwarenessContextUx)
+		return SetThreadDpiAwarenessContextUx(dpiContext);
+	return nullptr;
 }
 
 BOOL DcxDPIModule::dcxInheritWindowMonitor(HWND hwnd, HWND hwndInherit) noexcept
@@ -132,7 +379,21 @@ BOOL DcxDPIModule::dcxInheritWindowMonitor(HWND hwnd, HWND hwndInherit) noexcept
 	return FALSE;
 }
 
-DPI_HOSTING_BEHAVIOR DcxDPIModule::dcxSetThreadDpiHostingBehavior(DPI_HOSTING_BEHAVIOR value) noexcept
+BOOL DcxDPIModule::dcxIsValidDpiAwarenessContext(_In_ DPI_AWARENESS_CONTEXT value) noexcept
+{
+	if (IsValidDpiAwarenessContextUx)
+		return IsValidDpiAwarenessContextUx(value);
+	return FALSE;
+}
+
+BOOL DcxDPIModule::dcxSetDialogControlDpiChangeBehavior(HWND hWnd, _In_ DIALOG_CONTROL_DPI_CHANGE_BEHAVIORS mask, _In_ DIALOG_CONTROL_DPI_CHANGE_BEHAVIORS values) noexcept
+{
+	if (SetDialogControlDpiChangeBehaviorUx)
+		return SetDialogControlDpiChangeBehaviorUx(hWnd, mask, values);
+	return FALSE;
+}
+
+DPI_HOSTING_BEHAVIOR DcxDPIModule::dcxSetThreadDpiHostingBehavior(_In_ DPI_HOSTING_BEHAVIOR value) noexcept
 {
 	if (SetThreadDpiHostingBehaviorUx)
 		return SetThreadDpiHostingBehaviorUx(value);
--- DcxDPIModule.h
+++ DcxDPIModule.h
@@ -7,12 +7,31 @@
 class DcxDPIModule final
 	: public DcxModule
 {
+	static inline decltype(::AdjustWindowRectExForDpi)* AdjustWindowRectExForDpiUx = nullptr;
+	static inline decltype(::AreDpiAwarenessContextsEqual)* AreDpiAwarenessContextsEqualUx = nullptr;
+	static inline decltype(::EnableNonClientDpiScaling)* EnableNonClientDpiScalingUx = nullptr;
+	static inline decltype(::GetAwarenessFromDpiAwarenessContext)* GetAwarenessFromDpiAwarenessContextUx = nullptr;
+	static inline decltype(::GetDialogControlDpiChangeBehavior)* GetDialogControlDpiChangeBehaviorUx = nullptr;
+	static inline decltype(::GetDialogDpiChangeBehavior)* GetDialogDpiChangeBehaviorUx = nullptr;
+	static inline decltype(::GetDpiAwarenessContextForProcess)* GetDpiAwarenessContextForProcessUx = nullptr;
 	static inline decltype(::GetDpiForSystem)* GetDpiForSystemUx = nullptr;
-	static inline decltype(::GetDpiForWindow)* GetDpiForWindowUx= nullptr;
+	static inline decltype(::GetDpiForWindow)* GetDpiForWindowUx = nullptr;
+	static inline decltype(::GetDpiFromDpiAwarenessContext)* GetDpiFromDpiAwarenessContextUx = nullptr;
+	static inline decltype(::GetSystemDpiForProcess)* GetSystemDpiForProcessUx = nullptr;
 	static inline decltype(::GetSystemMetricsForDpi)* GetSystemMetricsForDpiUx = nullptr;
-	static inline decltype(::SetProcessDpiAwarenessContext)* SetProcessDpiAwarenessContextUx = nullptr;
+	static inline decltype(::GetThreadDpiAwarenessContext)* GetThreadDpiAwarenessContextUx = nullptr;
+	static inline decltype(::GetThreadDpiHostingBehavior)* GetThreadDpiHostingBehaviorUx = nullptr;
+	static inline decltype(::GetWindowDpiAwarenessContext)* GetWindowDpiAwarenessContextUx = nullptr;
+	static inline decltype(::GetWindowDpiHostingBehavior)* GetWindowDpiHostingBehaviorUx = nullptr;
 	static inline decltype(::InheritWindowMonitor)* InheritWindowMonitorUx = nullptr;
+	static inline decltype(::IsValidDpiAwarenessContext)* IsValidDpiAwarenessContextUx = nullptr;
+	static inline decltype(::SetDialogControlDpiChangeBehavior)* SetDialogControlDpiChangeBehaviorUx = nullptr;
+	static inline decltype(::SetDialogDpiChangeBehavior)* SetDialogDpiChangeBehaviorUx = nullptr;
+	static inline decltype(::SetProcessDpiAwarenessContext)* SetProcessDpiAwarenessContextUx = nullptr;
+	static inline decltype(::SetThreadCursorCreationScaling)* SetThreadCursorCreationScalingUx = nullptr;
+	static inline decltype(::SetThreadDpiAwarenessContext)* SetThreadDpiAwarenessContextUx = nullptr;
 	static inline decltype(::SetThreadDpiHostingBehavior)* SetThreadDpiHostingBehaviorUx = nullptr;
+	static inline decltype(::SystemParametersInfoForDpi)* SystemParametersInfoForDpiUx = nullptr;
 
 public:
 	constexpr DcxDPIModule(void) noexcept
@@ -23,17 +42,37 @@ public:
 	DcxDPIModule(const DcxDPIModule& other) = delete;	// no copy constructor
 	DcxDPIModule(const DcxDPIModule&& other) = delete;	// no move constructor
 	GSL_SUPPRESS(c.128) DcxDPIModule& operator =(const DcxDPIModule&) = delete;	// No assignments!
-	GSL_SUPPRESS(c.128) DcxDPIModule& operator =(const DcxDPIModule&&) = delete;	// No move assignments!
+	GSL_SUPPRESS(c.128) DcxDPIModule& operator =(DcxDPIModule&&) = delete;	// No move assignments!
 
 	bool load(void) final;
 	bool unload(void) noexcept final;
 
+	static BOOL dcxAdjustWindowRectExForDpi(_Inout_ LPRECT lpRect,_In_ DWORD dwStyle,_In_ BOOL bMenu,_In_ DWORD dwExStyle,_In_ UINT dpi) noexcept;
+	static BOOL dcxAreDpiAwarenessContextsEqual(_In_ DPI_AWARENESS_CONTEXT dpiContextA,_In_ DPI_AWARENESS_CONTEXT dpiContextB) noexcept;
+	static BOOL dcxEnableNonClientDpiScaling(_In_ HWND hwnd) noexcept;
+	static DPI_AWARENESS dcxGetAwarenessFromDpiAwarenessContext(_In_ DPI_AWARENESS_CONTEXT value) noexcept;
+	static DIALOG_CONTROL_DPI_CHANGE_BEHAVIORS dcxGetDialogControlDpiChangeBehavior(HWND hWnd) noexcept;
+	static DIALOG_DPI_CHANGE_BEHAVIORS dcxGetDialogDpiChangeBehavior(HWND hDlg) noexcept;
+	static DPI_AWARENESS_CONTEXT dcxGetDpiAwarenessContextForProcess(HANDLE hProcess) noexcept;
 	static UINT dcxGetDpiForSystem(void) noexcept;
 	static UINT dcxGetDpiForWindow(_In_ _Maybenull_ HWND hwnd) noexcept;
+	static UINT dcxGetDpiFromDpiAwarenessContext(_In_ DPI_AWARENESS_CONTEXT value) noexcept;
+	static UINT dcxGetSystemDpiForProcess(HANDLE hProcess) noexcept;
 	static UINT dcxGetSystemMetricsForDpi(_In_ int nIndex, _In_ UINT dpi) noexcept;
-	static UINT dcxGetWindowMetrics(_In_ _Maybenull_ HWND hwnd, _In_ int nIndex) noexcept;
-	static BOOL dcxSetProcessDpiAwarenessContext(_In_ DPI_AWARENESS_CONTEXT value) noexcept;
+	static DPI_AWARENESS_CONTEXT dcxGetThreadDpiAwarenessContext() noexcept;
+	static DPI_HOSTING_BEHAVIOR dcxGetThreadDpiHostingBehavior() noexcept;
+	static DPI_AWARENESS_CONTEXT dcxGetWindowDpiAwarenessContext(_In_ HWND hwnd) noexcept;
+	static DPI_HOSTING_BEHAVIOR dcxGetWindowDpiHostingBehavior(_In_ HWND hwnd) noexcept;
 	static BOOL dcxInheritWindowMonitor(_In_ HWND hwnd, _In_ HWND hwndInherit) noexcept;
+	static BOOL dcxIsValidDpiAwarenessContext(_In_ DPI_AWARENESS_CONTEXT value) noexcept;
+	static BOOL dcxSetDialogControlDpiChangeBehavior(HWND hWnd, _In_ DIALOG_CONTROL_DPI_CHANGE_BEHAVIORS mask, _In_ DIALOG_CONTROL_DPI_CHANGE_BEHAVIORS values) noexcept;
+	static BOOL dcxSetDialogDpiChangeBehavior(HWND hDlg, _In_ DIALOG_DPI_CHANGE_BEHAVIORS mask, _In_ DIALOG_DPI_CHANGE_BEHAVIORS values) noexcept;
+	static BOOL dcxSetProcessDpiAwarenessContext(_In_ DPI_AWARENESS_CONTEXT value) noexcept;
+	static UINT dcxSetThreadCursorCreationScaling(_In_ UINT cursorDpi) noexcept;
+	static DPI_AWARENESS_CONTEXT dcxSetThreadDpiAwarenessContext(_In_ DPI_AWARENESS_CONTEXT dpiContext) noexcept;
 	static DPI_HOSTING_BEHAVIOR dcxSetThreadDpiHostingBehavior(_In_ DPI_HOSTING_BEHAVIOR value) noexcept;
+
+	static BOOL dcxSystemParametersInfoForDpi(_In_ UINT uiAction,_In_ UINT uiParam,_Inout_ PVOID pvParam,_In_ UINT fWinIni,_In_ UINT dpi) noexcept;
+	static UINT dcxGetWindowMetrics(_In_ _Maybenull_ HWND hwnd, _In_ int nIndex) noexcept;
 };
 #endif // _DCXUXMODULES_H_
