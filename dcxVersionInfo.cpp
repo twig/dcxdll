33281ca6f4d6da69a4ae0b6b0d6bb63f6f7403fc
OokEek <aqss45@dsl.pipex.com>
2025-04-06 19:57:49 +0100

    [general] added version check for win10 build 18362+

--- dcxVersionInfo.cpp
+++ dcxVersionInfo.cpp
@@ -20,6 +20,23 @@ namespace
 
 		return VerifyVersionInfoW(&osvi, VER_MAJORVERSION | VER_MINORVERSION | VER_BUILDNUMBER, dwlConditionMask) != FALSE;
 	}
+	bool IsWindows10Build18362OrGreater() noexcept
+	{
+		constexpr WORD wMajorVersion = HIBYTE(_WIN32_WINNT_WINTHRESHOLD), wMinorVersion = LOBYTE(_WIN32_WINNT_WINTHRESHOLD), wBuildMajor = 18362ui16;
+		OSVERSIONINFOEXW osvi = { sizeof(osvi), 0, 0, 0, 0,{ 0 }, 0, 0 };
+		DWORDLONG        const dwlConditionMask = VerSetConditionMask(
+			VerSetConditionMask(
+				VerSetConditionMask(
+					0, VER_MAJORVERSION, VER_GREATER_EQUAL),
+				VER_MINORVERSION, VER_GREATER_EQUAL),
+			VER_BUILDNUMBER, VER_GREATER_EQUAL);
+
+		osvi.dwMajorVersion = wMajorVersion;
+		osvi.dwMinorVersion = wMinorVersion;
+		osvi.dwBuildNumber = wBuildMajor;
+
+		return VerifyVersionInfoW(&osvi, VER_MAJORVERSION | VER_MINORVERSION | VER_BUILDNUMBER, dwlConditionMask) != FALSE;
+	}
 }
 
 dcxVersionInfo::dcxVersionInfo() noexcept
@@ -28,5 +45,6 @@ dcxVersionInfo::dcxVersionInfo() noexcept
 	m_bWin7 = IsWindows7OrGreater();		// OS is Windows7+
 	m_bWin8 = IsWindows8OrGreater();		// OS is Windows8+
 	m_bWin10 = IsWindows10OrGreater();		// OS is Windows10+
+	m_bWin10NewerBuild = IsWindows10Build18362OrGreater();		// OS is Windows10 Build 18362+
 	m_bWin11 = IsWindows11OrGreater();		// OS is Windows11+
 }
--- dcxVersionInfo.h
+++ dcxVersionInfo.h
@@ -8,6 +8,7 @@ class dcxVersionInfo
 	bool m_bWin7{ false };
 	bool m_bWin8{ false };
 	bool m_bWin10{ false };
+	bool m_bWin10NewerBuild{ false };
 	bool m_bWin11{ false };
 
 public:
@@ -18,6 +19,7 @@ public:
 	const bool& isWin7(void) const noexcept { return m_bWin7; };
 	const bool& isWin8(void) const noexcept { return m_bWin8; };
 	const bool& isWin10(void) const noexcept { return m_bWin10; };
+	const bool& isWin10Build18362(void) const noexcept { return m_bWin10NewerBuild; };
 	const bool& isWin11(void) const noexcept { return m_bWin11; };
 };
 
