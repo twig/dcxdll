8fb3682aef0b7cf41b87dbe68c0f684df4af0716
OokEek <aqss45@dsl.pipex.com>
2026-07-08 02:34:14 +0100

    [xdock] removed some commented code.

--- Classes/UltraDock/dcxDock.h
+++ Classes/UltraDock/dcxDock.h
@@ -40,53 +40,6 @@ enum class DockFlags
 	DOCKF_UNDERSTATIC = 0x400,		//!< Keep the docked window under the parents static control. (auto sized to match width of static)
 	DOCKF_ABOVESTATIC = 0x800		//!< Keep the docked window above the parents static control. (auto sized to match width of static)
 };
-//template <typename T>
-//constexpr DockFlags& operator |=(DockFlags& eStyle, const T& dStyle) noexcept
-//{
-//	return eStyle = static_cast<DockFlags>(static_cast<DWORD>(eStyle) | static_cast<DWORD>(dStyle));
-//}
-//template <typename T>
-//constexpr DockFlags& operator &=(DockFlags& eStyle, const T& dStyle) noexcept
-//{
-//	return eStyle = static_cast<DockFlags>(static_cast<DWORD>(eStyle)& static_cast<DWORD>(dStyle));
-//}
-//template <typename T>
-//constexpr DockFlags operator &(const DockFlags& eStyle, const T& dStyle) noexcept
-//{
-//	return static_cast<DockFlags>(static_cast<DWORD>(eStyle)& static_cast<DWORD>(dStyle));
-//}
-//template <typename T>
-//constexpr DockFlags operator |(const DockFlags& eStyle, const T& dStyle) noexcept
-//{
-//	return static_cast<DockFlags>(static_cast<DWORD>(eStyle) | static_cast<DWORD>(dStyle));
-//}
-//constexpr DockFlags operator ~(const DockFlags& eStyle) noexcept
-//{
-//	return static_cast<DockFlags>(~static_cast<DWORD>(eStyle));
-//}
-
-//#define DOCKF_NORMAL					0x001	//!< No special flags.
-//#define DOCKF_AUTOH						0x002	//!< Auto Horizontal size.
-//#define DOCKF_AUTOV						0x004	//!< Auto Vertical size.
-//#define DOCKF_SIZE						0x008	//!< Auto Horizontal & Vertical size.
-//#define DOCKF_LEFT						0x010	//!< Dock to left. (UltraDock)
-//#define DOCKF_RIGHT						0x020	//!< Dock to right. (UltraDock)
-//#define DOCKF_TOP						0x040	//!< Dock to top. (UltraDock)
-//#define DOCKF_BOTTOM					0x080	//!< Dock to bottom. (UltraDock)
-//#define DOCKF_NOSCROLLBARS				0x100	//!< Disable parents scrollbars.
-//#define DOCKF_SHOWSCROLLBARS			0x200	//!< Stop the auto-sized window from covering the scrollbars of its parent.
-
-//#define TREEBAR_COLOUR_SELECTED			0
-//#define TREEBAR_COLOUR_SELECTED_BKG		1
-//#define TREEBAR_COLOUR_MESSAGE			2
-//#define TREEBAR_COLOUR_MESSAGE_BKG		3
-//#define TREEBAR_COLOUR_EVENT			4
-//#define TREEBAR_COLOUR_EVENT_BKG		5
-//#define TREEBAR_COLOUR_HIGHLIGHT		6
-//#define TREEBAR_COLOUR_HIGHLIGHT_BKG	7
-//#define TREEBAR_COLOUR_HOT_TEXT			8
-//#define TREEBAR_COLOUR_HOT_BKG			9
-//#define TREEBAR_COLOUR_MAX				9
 
 enum class TreeBarColours
 	: UINT
